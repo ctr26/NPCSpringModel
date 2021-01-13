e67ea46bd3aa7a0b5cc5890c52e54a8be1afa2c9
MariaTheiss <theiss@ebi.ac.uk>
2021-01-13 15:07:48 +0000

    bug in colorcoding fixed. Radial forces sampled from a gaussian. Magnitude of distortion can be set. Still a bug/issue with radial forces

--- NPC8nodes.py
+++ NPC8nodes.py
@@ -12,12 +12,25 @@ from scipy.integrate import odeint
 from scipy.linalg import circulant
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+import math
 
 def pol2cart(rho, phi):
     '''Function transforms polar to cartesian coordinates'''
     x = rho * np.cos(phi)
     y = rho * np.sin(phi)
     return(x, y)
+
+def rotate(point, angle): # From StackOverflow
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+    The angle should be given in radians.
+    """
+    ox, oy = (0., 0.) #origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
 
 def octagonspring(y, t, Lrest, la, K, ka, fext, d, n):
 
@@ -47,10 +60,8 @@ def octagonspring(y, t, Lrest, la, K, ka, fext, d, n):
     dxdt = np.concatenate((v.flatten(), allaccarray.flatten()))                                                                
     return dxdt
 
-### lengths of the spring
-la = 1 # radius hexadecagon (= length to anchor); set to 1
-
 # Generate cartesian coordinates of the Hexadecagon using its polar coordinates 
+la = 1 # radius hexadecagon (= length to anchor); set to 1
 angle = 0.
 cartc = np.zeros(16)
 
@@ -60,7 +71,7 @@ for i in range(0,16,2): # skip every other entry to populate it with y-coords
     cartc[i+1] = y
     angle += 0.25*np.pi
     
-cart2D = cartc.reshape(8,2) # reshape so that x and y coords are in separate columns. TODO 
+cart2D = cartc.reshape(8,2) # reshape so that x and y coords are in separate columns.  
 le = np.linalg.norm(cart2D[0,:]-cart2D[1,:]) # distance between node 0 and 1
 l2 = np.linalg.norm(cart2D[0,:]-cart2D[2,:])
 l3 = np.linalg.norm(cart2D[0,:]-cart2D[3,:])
@@ -73,23 +84,45 @@ Lrest = circulant([0, le, l2, l3, ld, l3, l2, le])
 ### constants of springs. numbers correspond to the numbering in lengths 
 ke = k2 = k3 = kd = 1 # spring constants 
 K = circulant([0, ke, k2, k3, kd, k3, k2, ke])
-ka = 0.3
+ka = 1
 
 # Other parameters
 d = 1 # damping 
-n = 3 # maximum distant neighbour to connect on each side 
+n = 2 # maximum distant neighbour to connect on each side 
 
-# External forces 
-fext = np.array([[5,-0.5]     ,   [0.,0.] ,   [0.,0.] ,   [0.,0.],
-                 [0.0,0.]   ,   [0.,0.] ,   [0.,0.] ,   [0.,0.]])
+### Sample forces
+#finalmag = -20.16112
+finalmag = -2
+fnorm = np.zeros((8,2))
+angle = 0.
+unitvector = np.array([1.,0.])
+for i in range(8):
+    norm = np.random.normal(0,1)
+    norm = 1 # TODO remove
+    fnorm[i] = rotate((norm*unitvector), angle)
+    angle += 0.25*np.pi
+
+#Determine total magnitude of distortions
+initialmag = 0.
+for i in range(len(fnorm)):
+    initialmag += np.linalg.norm(fnorm[i])
+
+
+magmultiplier = finalmag/initialmag
+
+fnorm = magmultiplier*fnorm
+
+# External forces, set manually if needed 
+fmanual = np.array([[0.,0.]     ,   [0.0,0.0] ,   [0.,0.] ,   [0.,0.2],
+                 [0.,0.]   ,   [0.,0.] ,   [0.,0.] ,   [0.,0.]])
 
 
 # starting values and timepoints 
 y0 = np.concatenate((cartc, np.zeros(16))) # last 16 entries are starting velocities 
-t_end = 50 # end timepoint
-t = np.linspace(0, t_end, t_end*12)
+t_end = 100
+t = np.linspace(0, t_end, t_end*100)
 
-sol16 = odeint(octagonspring, y0, t, args=(Lrest, la, K, ka, fext, d, n)) #TODO: try out adaptive timesteps 
+sol16 = odeint(octagonspring, y0, t, args=(Lrest, la, K, ka, fnorm, d, n)) #TODO: try out adaptive timesteps 
 solplot = sol16
 
 
@@ -104,9 +137,8 @@ for i in range(16):
 axs[0].set(xlabel = 't')
 axs[0].legend(loc = 'best')
 
-axs[1].plot(solplot[-1, np.append([i for i in range(0,16,2)],0)], solplot[-1, np.append([i for i in range(1,16,2)],1)], 
- 
-  linestyle = ":", marker = "o", color="gray", markerfacecolor = "black", markersize = 10)
+axs[1].plot(solplot[-1, np.append([i for i in range(0,16,2)],0)], solplot[-1, np.append([i for i in range(1,16,2)],1)],  
+  linestyle = ":", marker = "o", color="white", markerfacecolor = "darkgray", markersize = 10)
 
 colourcode = True
 if (colourcode ==False):
@@ -122,19 +154,17 @@ if (colourcode ==False):
 
 else:
     ### colourcoding velocities
-    #summarize velocities in x and y direction into one velocity 
-    pos = sol16[:,:16]
-    speeds = sol16[:,16:]
-    normspeeds = np.zeros((np.shape(speeds)[0],8))
+    pos = solplot[:,:16] # positions over time
+    vel = solplot[:,16:] # velocities over time
+    normvel = np.zeros((np.shape(vel)[0],8)) #shape: [steps,nodes]
     
     for node in range(0,15,2):    
-        for step in range(np.shape(speeds)[0]):
-            normspeeds[step,int(0.5*node)] = np.linalg.norm(speeds[step,node] - speeds[step,node+1])
-    
+        for step in range(np.shape(vel)[0]):
+             normvel[step,int(0.5*node)] = np.linalg.norm([vel[step,node], vel[step,node+1]])
     # plot trajectory
     #fig, axs = plt.subplots(1, 1, sharex=True, sharey=True)
     
-    norm = plt.Normalize(normspeeds.min(), normspeeds.max()) 
+    norm = plt.Normalize(normvel.min(), normvel.max()) 
     
     #####trajectory colorcoded for velocity
     for i in range(0,15,2):  
@@ -143,7 +173,7 @@ else:
         
     
         lc = LineCollection(segments, cmap = 'viridis', norm=norm)
-        lc.set_array(normspeeds[:,int(0.5*i)])
+        lc.set_array(normvel[:,int(0.5*i)])
         lc.set_linewidth(2)
         line = axs[1].add_collection(lc)
         
